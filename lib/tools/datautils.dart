f1ec4581733d69a4f6b61c3b29a3a6f479c606e7
KimWash <ckm0728wash@gmail.com>
2021-05-19 17:28:51 +0900

    Implement calendar month change, Update calendar

--- lib/tools/datautils.dart
+++ lib/tools/datautils.dart
@@ -4,3 +4,16 @@ lengthCheck(String value) {
   }
   return value;
 }
+
+int timeCheck(int hour) {
+  if (hour >= 0 && hour <= 8) {
+    return 0;
+  } else if (hour >= 9 && hour <= 13) {
+    return 1;
+  } else if (hour >= 14 && hour <= 18) {
+    return 2;
+  } else if (hour >= 19 && hour <= 0) {
+    return 3;
+  }
+  return null;
+}
